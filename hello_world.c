fe793b1aebc3baca679f1dc46aaa0a27e047c46f
Akul123 <luka.bajlo00@gmail.com>
2024-04-09 17:27:32 +0200

    added logging to syslog

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <syslog.h>
 
 int main(){
-	printf("HELLO WORLD!!!!\n");
+	openlog("hello_world_log_started", LOG_PID, LOG_USER);
+	
+	char* text = "HELLO WORLD!!!!\n";
+	printf("%s", text);
+	
+	syslog(LOG_INFO, "HELLO WORLD!!!!");
+	closelog();
 	return 1;
 };
